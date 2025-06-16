2471f85ad57ecba76fec5e75974d873249fed974
BastianCortez <bcortezh05@gmail.com>
2025-06-16 13:29:29 -0400

    Corrige formato de respuesta con make_response y jsonfy

--- functions/create_cliente_token/main.py
+++ functions/create_cliente_token/main.py
@@ -2,6 +2,7 @@ import logging
 import zcatalyst_sdk
 import uuid
 from datetime import date
+from flask import make_response, jsonify
 
 def handler(request):
     now = None
@@ -10,7 +11,7 @@ def handler(request):
         datastore = app.datastore()
         clientes = datastore.table("ClientesAPI")
 
-        # ✅ Detectar tipo de contenido y obtener los datos apropiadamente
+        # Soporte para JSON y formulario
         content_type = request.headers.get("Content-Type", "")
         if "application/json" in content_type:
             data = request.get_json()
@@ -21,10 +22,10 @@ def handler(request):
         rut = data.get("rut_empresa")
 
         if not nombre or not rut:
-            return {
+            return make_response(jsonify({
                 "status": "error",
                 "message": "Faltan campos requeridos"
-            }
+            }), 400)
 
         token = str(uuid.uuid4())
         now = date.today().isoformat()
@@ -39,14 +40,15 @@ def handler(request):
         }
 
         clientes.insert_row(row_data)
-        return {
+
+        return make_response(jsonify({
             "status": "success",
             "token_api": token
-        }
+        }), 200)
 
     except Exception as e:
-        return {
+        return make_response(jsonify({
             "status": "error",
             "message": str(e),
             "fecha_creacion_debug": str(now) if now else "now no definido"
-        }
+        }), 500)
